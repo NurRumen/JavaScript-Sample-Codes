c195161ded2dc39cee88aedf1cf68a756a0b5bd6
Gilfoyle <mnjnurrumen@gmail.com>
2019-09-26 00:05:42 +0600

    Initial Commit

---
+++ js_basics/js_basic_script.js
@@ -0,0 +1,11 @@
+console.log('Js Motherfucker is coming from another script.js!!');
+
+var motherFucker = 'This is a MF Script';
+var motherSucker = 'this is adding the Sucker Script!';
+var motherNothing = 'This shit is going to Be Pooped in the Alert Bitches';
+
+console.log(motherFucker + 'and' + motherSucker);
+alert(motherNothing);
+
+var motherPrompt = prompt('Does this MFs ass got Kicked?');
+console.log(motherPrompt);
